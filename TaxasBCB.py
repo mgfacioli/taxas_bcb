e3e8d6ba306799961bd6ed75fd793ae489fbc6e2
mgfacioli <mgfacioli@yahoo.com.br>
2021-10-11 16:23:29 -0300

    feita limpeza do codigo fonte

--- TaxasBCB.py
+++ TaxasBCB.py
@@ -13,8 +13,6 @@ Created on Fri Oct  8 14:12:23 2021
 ##############################################################################
 # importando modulos
 import pandas as pd
-#import matplotlib.pyplot as plt
-
 
 __author__ = """\n""".join(['Marcelo G Facioli (mgfacioli@yahoo.com.br)'])
 __version__ = "0.0.1"
@@ -30,6 +28,7 @@ class TaxasBcb(object):
             self.__data_inicio = None
             self.__data_fim = None
             self.__subtaxa = None
+            self.__acumular = None
 
 
     def __indice_acumulado(self, taxa):
@@ -42,6 +41,7 @@ class TaxasBcb(object):
         try:
             self.__data_inicio = data_inicio
             self.__data_fim = data_fim
+            self.__acumular = acumular
             taxa = pd.read_json(self._url)
             taxa['data'] = pd.to_datetime(taxa['data'], dayfirst=True)
             taxa.set_index('data', inplace=True)
@@ -57,7 +57,7 @@ class TaxasBcb(object):
                 else:
                     self.__subtaxa = taxa[pd.to_datetime(self.__data_inicio):pd.to_datetime(self.__data_fim)]
             
-            if acumular == True:
+            if self.__acumular:
                 self.__subtaxa = TaxasBcb.__indice_acumulado(self, self.__subtaxa.copy())
             
             return self.__subtaxa
@@ -68,23 +68,10 @@ class TaxasBcb(object):
             print(f"taxa {self._codigo} nao encontrado.")    
   
     def taxa_acumulada(self):
-        return round(((self.__subtaxa.iloc[-1][2])-1)*100, 4)
+        if self.__acumular:
+            return round(((self.__subtaxa.iloc[-1][2])-1)*100, 4)
+        else:
+            print('Para obter a taxa acumulada, o parametro acumular do metodo retorna_taxa deve ser True.')
 
-    
-# def grafico_linha(df, titulo, subtitulo, x, y, x_label, y_label):
-#     fig, ax = plt.subplots(1, figsize=(10,8), facecolor='#fccd68')
-#     ax.set_facecolor('#e7a2fc')
-#     #Hide the right and top spines
-#     ax.spines['right'].set_visible(False)
-#     ax.spines['top'].set_visible(False)
-#     #plt.ylim(0, 15)
-#     plt.title(f'{titulo}\n{subtitulo}', fontsize = 18,  c='k')
-#     plt.xlabel(f'\n{x_label}', fontsize = 14, c='k')
-#     plt.ylabel(f'\n{y_label}', fontsize = 14, c = 'k')
-#     line_1, = plt.plot(df.index, df[x], label = df.columns[0], linestyle='dashed')
-#     line_2, = plt.plot(df.index, df[y], label = df.columns[1], linestyle='dotted')
-#     plt.legend(handles=[line_1, line_2])
-#     plt.savefig('/media/mgfacioli/mgf32g/Python/Projetos/financas/financeiras/' + titulo + '.png')
 
-#     return None
    
--- teste.py
+++ teste.py
@@ -10,64 +10,19 @@ Created on Sun Oct 10 09:13:52 2021
 """
 
 import TaxasBCB
-import matplotlib.pyplot as plt
-
-
-def grafico_linha(df, titulo, subtitulo, x, y, x_label, y_label):
-    fig, ax = plt.subplots(1, figsize=(10,8), facecolor='#fccd68')
-    ax.set_facecolor('#e7a2fc')
-    #Hide the right and top spines
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    #plt.ylim(0, 15)
-    plt.title(f'{titulo}\n{subtitulo}', fontsize = 18,  c='k')
-    plt.xlabel(f'\n{x_label}', fontsize = 14, c='k')
-    plt.ylabel(f'\n{y_label}', fontsize = 14, c = 'k')
-    line_1, = plt.plot(df.index, df[x], label = df.columns[0], linestyle='dashed')
-    line_2, = plt.plot(df.index, df[y], label = df.columns[1], linestyle='dotted')
-    plt.legend(handles=[line_1, line_2])
-    plt.savefig('/media/mgfacioli/mgf32g/Python/Projetos/financas/financeiras/' + titulo + '.png')
-
-    return None
 
 
 ##############################################################################
 # Entrada do progrma
 def main():
-    # dt_inicio_consulta = '2021-01-01'
-    # dt_fim_consulta    = '2021-09-30'
-    
-    # # vamos comecar carregando o cdi do periodo em analise
-    # cdi = busca_taxa_bcb(12)
-    # selic = busca_taxa_bcb(11)
-    
-    # cdi.info()
-    # selic.info()
-    
-    # merge_outer = pd.merge(cdi, selic, how='outer', left_index=True, right_index=True)
-    # merge_inner = pd.merge(cdi, selic, how='inner', left_index=True, right_index=True)
-    # merge_inner.rename(columns={'valor_x': 'CDI', 'valor_y': 'Selic'}, inplace=True)
-    # merge_inner['diff'] = merge_inner['CDI'] - merge_inner['Selic']
-    
-    # grafico_linha(merge_inner, '01 CDI x Selic - Dia a Dia - 1986 a 2021', 'Fonte: Banco Central do Brasil', 'CDI', 'Selic', 'Data', 'CDI/Selic')
-    
-    # merge92x93x94 = merge_inner['1992':'1994']
-    
-    # grafico_linha(merge92x93x94, '02 CDI x Selic - Dia a Dia - 1992 a 1994', 'Fonte: Banco Central do Brasil', 'CDI', 'Selic', 'Data', 'CDI/Selic')
-    
-    # merge_2010x2021 = merge_inner['2010':'2021']
-
-    # grafico_linha(merge_2010x2021, '03 CDI x Selic - Dia a Dia - 2010 a 2021', 'Fonte: Banco Central do Brasil', 'CDI', 'Selic', 'Data', 'CDI/Selic')
-
-    
-    # resultado = merge_inner.groupby(merge_inner.index.year)['diff'].agg(['std', 'max', 'min'])
-
-    # resultado.to_csv('/media/mgfacioli/mgf32g/Python/Projetos/financas/financeiras/CDIxSelic.csv', sep = ';', decimal = ',')
 
     cdi = TaxasBCB.TaxasBcb()
     cdidf = cdi.retorna_taxa(data_inicio = '2021-01-01', data_fim= '2021-09-30')
     cdi._url
     cdi.taxa_acumulada()
+
+    cdi2020 = cdi.retorna_taxa('2020-01-01', '2020-12-31', acumular=True)
+    cdi.taxa_acumulada()    
 
 
     ipca = TaxasBcb(codigo=433)
